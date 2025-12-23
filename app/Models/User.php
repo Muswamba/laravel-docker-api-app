8549d76001415c49d14875193c3ca996cdc6e288
Shabani Muswamba <37952209+Muswamba@users.noreply.github.com>
2025-12-23 19:28:41 +0300

    Add refreshTokens relationship to User model
    Added refreshTokens relationship to User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Auth\RefreshToken;
+
 
 class User extends Authenticatable
 {
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function refreshTokens()
+    {
+        return $this->hasMany(RefreshToken::class);
     }
 }
